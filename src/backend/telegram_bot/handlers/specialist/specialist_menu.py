100c5e964cea3902016e6add9de5e43a0cc92ea4
Yuriy Bandura <bandurayv@gmail.com>
2025-02-18 13:04:53 +0300

    Add_New_Jazz

--- src/backend/telegram_bot/handlers/specialist/specialist_menu.py
+++ src/backend/telegram_bot/handlers/specialist/specialist_menu.py
@@ -602,4 +602,6 @@ async def handle_specialist_cancel_complete(update: Update, context: ContextType
         "Вы можете снова выбрать действие в меню.",
         parse_mode="Markdown"
     )
-# /b Jazz
+
+
+# /b Jazz 123
